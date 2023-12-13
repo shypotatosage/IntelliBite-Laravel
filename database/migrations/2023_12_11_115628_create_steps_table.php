49f4761ee3784f9425148d33239195da07566154
shypotatosage <kathyjiegraal@gmail.com>
2023-12-13 16:13:54 +0700

    Update 2023_12_11_115628_create_steps_table.php

--- database/migrations/2023_12_11_115628_create_steps_table.php
+++ database/migrations/2023_12_11_115628_create_steps_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
-            $table->string('description');
+            $table->text('description');
             $table->foreignIdFor(Recipe::class);
             $table->timestamps();
             
