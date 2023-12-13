0abbfc02e248a3ad6947db3c9b559b58e0839e8f
shypotatosage <kathyjiegraal@gmail.com>
2023-12-13 14:34:49 +0700

    Update 2023_12_11_115628_create_steps_table.php

--- database/migrations/2023_12_11_115628_create_steps_table.php
+++ database/migrations/2023_12_11_115628_create_steps_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Recipe;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,10 @@ return new class extends Migration
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
             $table->string('description');
+            $table->foreignIdFor(Recipe::class);
             $table->timestamps();
+            
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
